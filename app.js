1b435f3bd93e82a6560f14a250b134bc1c223d34
harish0775 <harish.nishad0775@gmail.com>
2022-04-10 17:28:19 +0530

    Shop

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const errorMiddleware = require("./middlewares/errors");
+const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
+const fileupload = require("express-fileupload");
+const path = require("path");
+
+let cors = require("cors");
+
+//setting up config file
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileupload());
+app.use(cors());
+
+//setting up cloudinary config
+
+//import all routes
+const products = require("./routes/product");
+const auth = require("./routes/auth");
+const order = require("./routes/order");
+const payment = require("./routes/payment");
+app.use("/api/v1", products);
+app.use("/api/v1", auth);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
+
+//error middleware
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ routes/product.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getProduct,
+  newProduct,
+  getsingleProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getProductReview,
+  deleteReview,
+  getAdminProducts,
+} = require("../controllers/productController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+
+router.route("/products").get(getProduct);
+router.route("/admin/products").get(getAdminProducts);
+router.route("/product/:id").get(getsingleProduct);
+
+router
+  .route("/admin/products/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+
+router
+  .route("/reviews")
+  .post(isAuthenticatedUser, createProductReview)
+  .get(isAuthenticatedUser, getProductReview)
+  .delete(isAuthenticatedUser, deleteReview);
+
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(deleteProduct);
+module.exports = router;
